c115729dc0564a017c243177c3a4b0776a41e7bc
Lucas Bispo <skaliboo1@gmail.com>
2022-11-28 19:18:54 -0300

    add functions

---
+++ 03-functions.py
@@ -0,0 +1,18 @@
+# função basica em python
+
+
+def i_am():
+    print("eu sou uma função ")
+
+
+i_am()
+
+# essa função contem parametros e retorno potanto
+# é nesscessario executar a função dentro do print
+
+
+def sum(number_1, number_2):
+    return number_1 + number_2
+
+
+print(sum(1, 2))
